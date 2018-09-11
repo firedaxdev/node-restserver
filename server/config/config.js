1429b8840ccccfd867e3cf5be5f593f59254c87f
Armando <firedax01@gmail.com>
2018-09-10 22:28:19 -0600

    REST Server Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,21 @@
 // ====================
 
 process.env.PORT = process.env.PORT || 3000
+
+// ====================
+// ENTORNO
+// ====================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ====================
+// BASE DE DATOS
+// ====================
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb://cafe-user:Armando68.@ds251622.mlab.com:51622/cafe'
+}
+process.env.URLDB = urlDB;
